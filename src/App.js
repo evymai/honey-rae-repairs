e503149d0a2958efc8967ec8ee838141dbc5daea
Emily Mai <emily.vy.mai@gmail.com>
2024-01-05 09:18:37 -0600

    part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import { getAllTickets } from "./services/ticketServices"
+import "./App.css"
+export const App = () => {
+  //define all states
+  const [allTickets, setAllTickets] = useState([])
+  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
+  const [filteredTickets, setFilteredTickets] = useState([])
 
-function App() {
+  //useEffects
+  useEffect(() => {
+    getAllTickets().then((ticketsArray) => {
+      setAllTickets(ticketsArray)
+      console.log("tickets set!")
+    })
+  }, []) // ONLY runs on initial render of component
+
+  useEffect(() => {
+    if (showEmergencyOnly) {
+      //set emergencyTickets to emergency tickets only
+      const emergencyTickets = allTickets.filter(
+        (ticket) => ticket.emergency === true
+      )
+      setFilteredTickets(emergencyTickets)
+    } else {
+      setFilteredTickets(allTickets)
+    }
+    console.log("show emergency changed")
+  }, [showEmergencyOnly, allTickets]) //depends on allTickets and showEmergencyOnly
+
+  //render JSX
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="tickets-container">
+      <h2>Tickets</h2>
+      <div>
+        <button
+          className="filter-btn btn-primary"
+          onClick={() => {
+            setShowEmergencyOnly(true)
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Emergency
+        </button>
+        <button
+          className="filter-btn btn-info"
+          onClick={() => {
+            setShowEmergencyOnly(false)
+          }}
+        >
+          Show All
+        </button>
+      </div>
+      <article className="tickets">
+        {filteredTickets.map((ticket) => {
+          return (
+            <section className="ticket" key={ticket.id}>
+              <header className="ticket-info">#{ticket.id}</header>
+              <div>{ticket.description}</div>
+              <footer>
+                <div>
+                  <div className="ticket-info">emergency</div>
+                  <div>{ticket.emergency ? "yes" : "no"}</div>
+                </div>
+              </footer>
+            </section>
+          )
+        })}
+      </article>
     </div>
-  );
+  )
 }
-
-export default App;
